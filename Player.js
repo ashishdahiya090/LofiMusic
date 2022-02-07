0d9705607b53a821d393e58b092088ea889157d9
ASHISH DAHIYA <ashishdahiya090@gmail.com>
2022-02-08 01:09:57 +0530

    Fixed volume range

--- Player.js
+++ Player.js
@@ -1,10 +1,6 @@
 
 var audio = new Audio('Songs/testSong.mp3');
 var Volu=100;
-var vol=document.getElementById("Vol_Input");
-if(vol) {
-    vol.oninput = Volume
-}
 var IsMuted = false;
 function random(min,max){
    return  Math.floor(Math.random()*(max-min+1)+min);
@@ -20,20 +16,29 @@ function Pause(){
 function Mute(){
     if(!IsMuted){
         document.getElementById("Vol_Input").value ="0";
-        console.log("audio muted");
         document.getElementById("Mute").src="Mute.png";
+        Volume(0);
         IsMuted=true;
+        console.log("audio muted");
+
     }
     else {
         document.getElementById("Vol_Input").value=Volu;
-        console.log("audio unMuted");
         document.getElementById("Mute").src="Sound.png";
+        Volume(Volu/100);
         IsMuted=false;
+        console.log("audio unMuted");
+
     }
 }
+function VolumeRange(){
+    var vol=document.getElementById("Vol_Input").value;
+        console.log(vol);
+        Volu=vol;
+        Volume(vol/100);
 
-function Volume(){
-    audio.volume=vol.value/100 ;
-    Volu = vol.value;
-    console.log(vol.value/100);
+}
+function Volume(volume){
+    audio.volume=volume ;
+    console.log(volume);
 }
